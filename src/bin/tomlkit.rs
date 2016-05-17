0643cc8d3719cd0a052fc505adf5afe7ccb7cc21
Joel Self <joel@dislocal.com>
2016-05-16 19:40:21 -0600

    Added a print-doc command, added and implemnted a file argument since pirate doesn't have positional arguments. Implemented the get_file function. Added all other stub methods.

--- src/bin/tomlkit.rs
+++ src/bin/tomlkit.rs
@@ -1,9 +1,16 @@
 extern crate pirate;
-use pirate::{Matches, Match, Vars, matches, usage, vars};
+extern crate tomllib;
+use std::fs::File;
 use std::env;
+use std::io::{Read, BufReader, Error};
+use pirate::{Matches, Match, Vars, matches, usage, vars};
+use tomllib::TOMLParser;
+use tomllib::types::ParseResult;
 
 fn main() {
   let options = vec![
+    "Required arguments",
+    "f/file#The path to the TOML document to parse and manipulate.:",
     "#File-preserving commands",
     "g/get-value#Given a key or comma separated list of keys, retrieve the values for those keys. If multiple keys are \
       specified and any one of them fails to retrieve a value, the whole command will fail with an error message.:",
@@ -21,12 +28,14 @@ fn main() {
       specified value. For instance: \"foo.bar,hello,basic-string,baz.qux,82374,int\" will set the key \"foo.bar\" to \
       a basic string value of \"hello\" and the key \"baz.qux\" to an integer value of 82374. If multiple keys are \
       specified and any one of them fails to set a value, the whole command will fail with an error message.:",
-    "#Options.",
+    "#Pre-command Options.",
     "h/help#Show this screen.",
     "/set-true#For commands that print \"true\" or \"false\", this will change what value is printed for \"true\", \
       i.e. --set-true=1 will cause tomlkit to print \"1\" for \"true\" instead of \"true\".:",
     "/set-false#For commands that print \"true\" or \"false\", this will change what value is printed for \"false\", \
       i.e. --set-false=0 will cause tomlkit to print \"0\" for \"false\" instead of \"false\".:",
+    "#Post-command Options.",
+    "/print-doc#Print out the resultant TOML document after all requested changes have been made.",
   ];
 
   let mut vars: Vars = match vars("tomlkit", &options) {
@@ -48,24 +57,125 @@ fn main() {
     usage(&vars);
     return;
   }
+  let mut true_val = &"true".to_string();
+  let mut false_val = &"false".to_string();
+  let mut output: String = "".to_string();
 
-  // Switch statement
-  if matches.has_match("get-value") {
-    println!("You requested get-value: {}", matches.get("get-value").unwrap());
-  } else if matches.has_match("has-value") {
-    println!("You requested has-value: {}", matches.get("has-value").unwrap());
-  } else if matches.has_match("get-children") {
-    println!("You requested get-children: {}", matches.get("get-children").unwrap());
-  } else if matches.has_match("has-children") {
-    println!("You requested has-children: {}", matches.get("has-children").unwrap());
-  } else if matches.has_match("set-value") {
-    println!("You requested set-value: {}", matches.get("set-value").unwrap());
-  } else if matches.has_match("set-true") {
-    println!("You requested set-true: {}", matches.get("set-true").unwrap());
+  // The file we're operating on
+  let mut file = String::new();
+  if matches.has_match("file") {
+    if let Some(f) = matches.get("file") {
+      match get_file(f, &mut file) {
+        Err(err) => {
+          println!("Error: Unable to read file: \"{}\". Reason: {}", f, err);
+          return;
+        },
+        _ => ()
+      }
+    } else {
+      println!("Error: A required argument is missing for f/file.");
+      usage(&vars);
+    }
+  } else
+
+  // Pre-command options
+  if matches.has_match("set-true") {
+    if let Some(t) = matches.get("set-true") {
+      true_val = t;
+    } else {
+      println!("Error: A required argument is missing for set-true.");
+      usage(&vars);
+    }
   } else if matches.has_match("set-false") {
-    println!("You requested set-false: {}", matches.get("set-false").unwrap());
+    if let Some(f) = matches.get("set-false") {
+      false_val = f;
+    } else {
+      println!("Error: A required argument is missing for set-false.");
+      usage(&vars);
+    }
+  }
+
+  // Commands only one command allowed per invocation for this version
+  if matches.has_match("get-value") {
+    if let Some(k) = matches.get("get-value") {
+      output = get_value(k);
+    } else {
+      println!("Error: A required argument is missing for g/get-value.");
+      usage(&vars);
+    }
+  } else if matches.has_match("has-value") {
+    if let Some(k) = matches.get("has-value") {
+      output = has_value(k, true_val, false_val);
+    } else {
+      println!("Error: A required argument is missing for has-value.");
+      usage(&vars);
+    }
+  } else if matches.has_match("get-children") {
+    if let Some(v) = matches.get("get-children") {
+      output = get_children(v);
+    } else {
+      println!("Error: A required argument is missing for c/get-children.");
+      usage(&vars);
+    }
+  } else if matches.has_match("has-children") {
+    if let Some(k) = matches.get("has-children") {
+      output = has_children(k, true_val, false_val);
+    } else {
+      println!("Error: A required argument is missing for has-children.");
+      usage(&vars);
+    }
+  } else if matches.has_match("set-value") {
+    if let Some(kv) = matches.get("set-value") {
+      output = set_value(kv);
+    } else {
+      println!("Error: A required argument is missing for s/set-value.");
+      usage(&vars);
+    }
   } else {
     // No command specified print usage
     usage(&vars);
+    return;
   }
+
+  // Post-command options
+  if matches.has_match("print-doc") {
+    print_doc(&output);
+  }
+}
+
+fn get_file(file_path: &String, out_file: &mut String) -> Result<usize, Error> {
+  match File::open(file_path) {
+    Ok(file) => {
+      let mut contents = BufReader::new(&file);
+      match contents.read_to_string(out_file) {
+        Ok(bytes) => return Ok(bytes),
+        Err(err) => return Err(err),
+      }
+    },
+    Err(err) => return Err(err),
+  }
+}
+
+fn get_value(key: &String) -> String {
+  unimplemented!();
+}
+
+fn has_value(key: &String, true_val: &String, false_val: &String) -> String {
+  unimplemented!();
+}
+
+fn get_children(key: &String) -> String {
+  unimplemented!();
+}
+
+fn has_children(key: &String, true_val: &String, false_val: &String) -> String {
+  unimplemented!();
+}
+
+fn set_value(key: &String) -> String {
+  unimplemented!();
+}
+
+fn print_doc(doc: &String) {
+  unimplemented!();
 }
