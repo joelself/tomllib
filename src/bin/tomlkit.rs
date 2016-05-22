e64e639dc6c12b591b6b179f671771ca63a065d6
Joel Self <self@jself.io>
2016-05-22 17:37:31 -0600

    Added array-begin array-end array-separator, array-length, and strip-quotes options. Implemented each of the new options. Moved options to their own struct so that they don't have to be queried over and over.

--- src/bin/tomlkit.rs
+++ src/bin/tomlkit.rs
@@ -54,7 +54,23 @@ fn main() {
       i.e. --set-true=1 will cause tomlkit to print \"1\" for \"true\" instead of \"true\".:",
     "/set-false#For commands that print \"true\" or \"false\", this will change what value is printed for \"false\", \
       i.e. --set-false=0 will cause tomlkit to print \"0\" for \"false\" instead of \"false\".:",
-    "p/separator#Set the string that will separate multiple results. The default is \" ,\".:",
+    "p/separator#Set the string that will separate multiple results. The default is \", \".:",
+    "array-separator#Set the string that will separate multiple results within an array. The default is \", \". NOTE! \
+    This option is likely to go away in version 0.2.0 in favor of specifying a format string.:",
+    "array-begin#Set the string that will begin a set of results in an array. The default is \"[\". **NOTE!** \
+    This option is likely to go away in version 0.2.0 in favor of specifying a format string.:",
+    "array-end#Set the string that will end a set of results in an array. The default is \"]\". **NOTE!** \
+    This option is likely to go away in version 0.2.0 in favor of specifying a format string.:",
+    "array-separator#Set the string that will separate multiple results within an array. The default is \", \". \
+    **NOTE!** This option is likely to go away in version 0.2.0 in favor of specifying a format string.:",
+    "array-length#Rather than beginning and ending an array with special characters, print the length of the array \
+    as an unsigned 64 bit integer before printing the contents of the array, e.g. 3, \"Alice\", \"Bob\", \"Eve\".",
+    "strip-quotes#All types of string values will be printed without single or double quotes. \
+    **NOTE!** This option is likely to go away in version 0.2.0 in favor of specifying a format string.:",
+    "/strip-quotes#Removes all single and double quotes from returned string values. It does not strip quotes from \
+    keys as that would make them unusable. Warning: This will make it impossible to distinguish between each type of \
+    string and it can make it impossible to tell the return type e.g. Is 1234 without quotes an integer or some kind \
+    of string? Is true a string or a boolean?",
     "q/quiet#For commands that modify rather than return a result, turn off printing \"Success\" for each successful \
     modification.",
     "#Post-command Options",
@@ -84,10 +100,11 @@ fn main() {
     usage!(&vars);
   }
 
+  let opts = Options::new(&matches, &vars);
   // The file we're operating on
   if matches.has_match("input-file") {
     if let Some(f) = matches.get("input-file") {
-      process_document(f, &matches, &vars);
+      process_document(f, &opts, &matches, &vars);
     } else {
       usage!(println!("Error: A required argument is missing for input-file."), &vars);
     }
@@ -100,7 +117,7 @@ fn main() {
           if n == 0 {
             break;
           }
-          process_document(&input.trim().to_string(), &matches, &vars);
+          process_document(&input.trim().to_string(), &opts, &matches, &vars);
           input.clear();
         },
         Err(err) => {
@@ -112,12 +129,84 @@ fn main() {
   }
 }
 
-fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
-  let mut true_val = &"true".to_string();
-  let mut false_val = &"false".to_string();
-  let mut separator = &", ".to_string();
-  let mut quiet = false;
-  let mut file = String::new();
+struct Options {
+  true_vals: String,
+  false_vals: String,
+  separator: String,
+  arr_start: String,
+  arr_end: String,
+  arr_sep: String,
+  quiet: bool,
+  arr_len: bool,
+  strip_quotes: bool,
+  print_doc: bool,
+}
+
+impl Options {
+  fn new(matches: &Matches, vars: &Vars) -> Options {
+    let mut opts = Options{true_vals: "true".to_string(), false_vals: "false".to_string(),
+                   separator: ", ".to_string(), arr_start: "[".to_string(),
+                   arr_end: "]".to_string(), arr_sep: ", ".to_string(),
+                   quiet: false, arr_len: false, strip_quotes: false, print_doc: false};
+    // Pre-command options
+    if matches.has_match("set-true") {
+      if let Some(t) = matches.get("set-true") {
+        opts.true_vals = t.clone();
+      } else {
+        usage!(println!("Error: A required argument is missing for set-true."), &vars);
+      }
+    }
+    if matches.has_match("set-false") {
+      if let Some(f) = matches.get("set-false") {
+        opts.false_vals = f.clone();
+      } else {
+        usage!(println!("Error: A required argument is missing for set-false."), &vars);
+      }
+    }
+    if matches.has_match("separator") {
+      if let Some(s) = matches.get("separator") {
+        opts.separator = s.clone();
+      } else {
+        usage!(println!("Error: A required argument is missing for separator."), &vars);
+      }
+    }
+    if matches.has_match("array-begin") {
+      if let Some(b) = matches.get("array-begin") {
+        opts.arr_start = b.clone();
+      } else {
+        usage!(println!("Error: A required argument is missing for array-begin."), &vars);
+      }
+    }
+    if matches.has_match("array-end") {
+      if let Some(e) = matches.get("array-end") {
+        opts.arr_end = e.clone();
+      } else {
+        usage!(println!("Error: A required argument is missing for separator."), &vars);
+      }
+    }
+    if matches.has_match("array-separator") {
+      if let Some(s) = matches.get("array-separator") {
+        opts.arr_sep = s.clone();
+      } else {
+        usage!(println!("Error: A required argument is missing for separator."), &vars);
+      }
+    }
+    if matches.has_match("array-length") {
+      opts.arr_len = true;
+    }
+    if matches.has_match("strip-quotes") {
+      opts.strip_quotes = true;
+    }
+    // Post-command options
+    if matches.has_match("print-doc") {
+      opts.print_doc = true;
+    }
+    return opts;
+  }
+}
+
+fn process_document(file_path: &String, opts: &Options, matches: &Matches, vars: &Vars) {
+  let mut file: String = "".to_string();
 
   match get_file(file_path, &mut file)  {
     Ok(()) => (),
@@ -152,37 +241,15 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
     _ => (), // If verbose output Full or FullError
   }
 
-  // Pre-command options
-  if matches.has_match("set-true") {
-    if let Some(t) = matches.get("set-true") {
-      true_val = t;
-    } else {
-      usage!(println!("Error \"{}\": A required argument is missing for set-true.", file_path), &vars);
-    }
-  }
-  if matches.has_match("set-false") {
-    if let Some(f) = matches.get("set-false") {
-      false_val = f;
-    } else {
-      usage!(println!("Error \"{}\": A required argument is missing for set-false.", file_path), &vars);
-    }
-  }
-  if matches.has_match("separator") {
-    if let Some(s) = matches.get("separator") {
-      separator = s;
-    } else {
-      usage!(println!("Error \"{}\": A required argument is missing for separator.", file_path), &vars);
-    }
-  }
-
   let mut command: bool = false;
   let mut result: Vec<Result<String, String>> = vec![Ok("".to_string())];
   let mut out_file = file_path;
+  let mut hasval_keycount = 0;
   // Commands only one command allowed per invocation for this version
   if matches.has_match("get-value") {
     command = true;
     if let Some(k) = matches.get("get-value") {
-      result.push(get_value(k, separator, &parser));
+      result.push(get_value(k, &opts.separator, opts.strip_quotes, &parser));
     } else {
       usage!(println!("Error \"{}\": A required argument is missing for g/get-value.", file_path), &vars);
     }
@@ -191,7 +258,7 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
     if matches.has_match("has-value") {
       command = true;
       if let Some(k) = matches.get("has-value") {
-        result.push(has_value(k, separator, &parser, true_val, false_val));
+        result.push(has_value(k, &opts.separator, &opts.true_vals, &opts.false_vals, &mut hasval_keycount, &parser));
       } else {
         usage!(println!("Error \"{}\": A required argument is missing for has-value.", file_path), &vars);
       }
@@ -201,7 +268,8 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
     if matches.has_match("get-children") {
       command = true;
       if let Some(k) = matches.get("get-children") {
-        result.push(get_children(k, separator, &parser));
+        result.push(get_children(k, &opts.separator, &opts.arr_start, &opts.arr_end, &opts.arr_sep, opts.arr_len,
+          &parser));
       } else {
         usage!(println!("Error \"{}\": A required argument is missing for c/get-children.", file_path), &vars);
       }
@@ -211,7 +279,7 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
     if matches.has_match("has-children") {
       command = true;
       if let Some(k) = matches.get("has-children") {
-        result.push(has_children(k, separator, &parser, true_val, false_val));
+        result.push(has_children(k, &opts.separator, &opts.true_vals, &opts.false_vals, &hasval_keycount, &parser));
       } else {
         usage!(println!("Error \"{}\": A required argument is missing for has-children.", file_path), &vars);
       }
@@ -221,10 +289,7 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
     if matches.has_match("set-value") {
       command = true;
       if let Some(kv) = matches.get("set-value") {
-        if  matches.has_match("quiet") {
-          quiet = true;
-        }
-        result.push(set_value(kv, separator, quiet, &mut parser));
+        result.push(set_value(kv, &opts.separator, opts.quiet, &mut parser));
         if matches.has_match("output-file") {
           match matches.get("output-file") {
             Some(out) => out_file = out,
@@ -258,7 +323,7 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
   for i in 0..result.len() {
     match &result[i]  {
       &Ok(ref val) => {
-        if !quiet {
+        if !opts.quiet {
           print!("{}", val);
         }
       },
@@ -267,7 +332,7 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
       }
     }
     if i < result.len() - 1 {
-      print!("{}", separator);
+      print!("{}", opts.separator);
     }
     if i == result.len() - 1 {
       println!("");
@@ -275,9 +340,7 @@ fn process_document(file_path: &String, matches: &Matches, vars: &Vars) {
   }
 
   // Post-command options
-  if matches.has_match("print-doc") {
-    println!("\n||  ||  ||  ||  ||  ||  ||  ||  ||  DOCUMENT  ||  ||  ||  ||  ||  ||  ||  ||  ||");
-    println!("\\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/            \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/\n");
+  if opts.print_doc {
     print_doc(&parser);
   }
 }
@@ -295,7 +358,7 @@ fn get_file(file_path: &String, out_file: &mut String) -> Result<(), Error> {
   Ok(())
 }
 
-fn get_value(csv: &str, sep: &String, doc: &TOMLParser) -> Result<String, String> {
+fn get_value(csv: &str, sep: &String, strip_quotes: bool, doc: &TOMLParser) -> Result<String, String> {
   let key_results = csv_to_vec(csv);
   if let Ok(keys) = key_results {
     if keys.len() == 0 {
@@ -305,7 +368,11 @@ fn get_value(csv: &str, sep: &String, doc: &TOMLParser) -> Result<String, String
     for i in 0..keys.len() {
       let key: &str = &keys[i];
       if let Some(value) = doc.get_value(key) {
-        result.push_str(&format!("{}", value));
+        if strip_quotes {
+          result.push_str(&format!("{}", value).trim_matches(|c| c == '\'' || c == '\"'));
+        } else {
+          result.push_str(&format!("{}", value));
+        }
         if i < keys.len() - 1 {
           result.push_str(sep);
         }
@@ -318,20 +385,42 @@ fn get_value(csv: &str, sep: &String, doc: &TOMLParser) -> Result<String, String
   Err(format!("Could not parse keys: \"{:?}\".", csv))
 }
 
-fn has_value(csv: &str, sep: &String, doc: &TOMLParser, true_val: &String, false_val: &String)
+fn has_value(csv: &str, sep: &String, true_vals: &String, false_vals: &String, keycount: &mut usize, doc: &TOMLParser)
   -> Result<String, String> {
   let key_results = csv_to_vec(csv);
+  let true_results = csv_to_vec(true_vals);
+  let false_results = csv_to_vec(false_vals);
   if let Ok(keys) = key_results {
     if keys.len() == 0 {
       return Err(format!("No keys specified: \"{}\".", csv));
     }
+    *keycount = keys.len();
     let mut result = String::new();
     for i in 0..keys.len() {
+      let (true_val, false_val);
+      if let Ok(ref true_vals) = true_results {
+        if true_vals.len() > 1 {
+          true_val = &true_vals[i];
+        } else {
+          true_val = &true_vals[0];
+        }
+      } else {
+        return Err(format!("Invalid set of true values specified: \"{}\".", true_vals));
+      };
+      if let Ok(ref false_vals) = false_results {
+        if false_vals.len() > 1 {
+          false_val = &false_vals[i];
+        } else {
+          false_val = &false_vals[0];
+        }
+      } else {
+        return Err(format!("Invalid set of false values specified: \"{}\".", false_vals));
+      }
       let key: &str = &keys[i];
       if let Some(_) = doc.get_value(key) {
-        result.push_str(true_val);
+        result.push_str(&true_val);
       } else {
-        result.push_str(false_val);
+        result.push_str(&false_val);
       }
       if i < keys.len() - 1 {
         result.push_str(sep);
@@ -342,7 +431,8 @@ fn has_value(csv: &str, sep: &String, doc: &TOMLParser, true_val: &String, false
   Err(format!("Could not parse keys: \"{}\".", csv))
 }
 
-fn get_children(csv: &str, sep: &String, doc: &TOMLParser) -> Result<String, String> {
+fn get_children(csv: &str, sep: &String, arr_start: &String, arr_end: &String, arr_sep: &String, arr_len: bool,
+  doc: &TOMLParser) -> Result<String, String> {
   let key_results = csv_to_vec(csv);
   if let Ok(keys) = key_results {
     if keys.len() == 0 {
@@ -355,15 +445,22 @@ fn get_children(csv: &str, sep: &String, doc: &TOMLParser) -> Result<String, Str
         match c {
           &Children::Keys(ref ckeys) => {
             let mut val = String::new();
-            val.push('[');
+            if arr_len {
+              val.push_str(&format!("{}", ckeys.borrow().len()));
+              val.push_str(arr_sep);
+            } else {
+              val.push_str(arr_start);
+            }
             if ckeys.borrow().len() > 0 {
               for i in 0..ckeys.borrow().len() - 1 {
                 val.push_str(&ckeys.borrow()[i]);
-                val.push_str(", ");
+                val.push_str(arr_sep);
               }
               val.push_str(&ckeys.borrow()[ckeys.borrow().len() - 1]);
             }
-            val.push(']');
+            if !arr_len {
+              val.push_str(arr_end);
+            }
             result.push_str(&val);
           },
           &Children::Count(ref size) => {
@@ -385,20 +482,56 @@ fn get_children(csv: &str, sep: &String, doc: &TOMLParser) -> Result<String, Str
   Err(format!("Could not parse keys: \"{}\".", csv))
 }
 
-fn has_children(csv: &str, sep: &String, doc: &TOMLParser, true_val: &String, false_val: &String)
+fn has_children(csv: &str, sep: &String, true_vals: &String, false_vals: &String, keycount: &usize, doc: &TOMLParser)
   -> Result<String, String> {
   let key_results = csv_to_vec(csv);
+  let true_results = csv_to_vec(true_vals);
+  let false_results = csv_to_vec(false_vals);
   if let Ok(keys) = key_results {
     if keys.len() == 0 {
       return Err(format!("No keys specified: \"{}\".", csv));
     }
     let mut result = String::new();
     for i in 0..keys.len() {
-      let key: &str = &keys[i];
-      if let Some(_) = doc.get_children(key) {
-        result.push_str(true_val);
+      let (true_val, false_val);
+      if let Ok(ref true_vals) = true_results {
+        if true_vals.len() > 1 {
+          true_val = &true_vals[i+keycount];
+        } else {
+          true_val = &true_vals[0];
+        }
       } else {
-        result.push_str(false_val);
+        return Err(format!("Invalid set of true values specified: \"{}\".", true_vals));
+      };
+      if let Ok(ref false_vals) = false_results {
+        if false_vals.len() > 1 {
+          false_val = &false_vals[i+keycount];
+        } else {
+          false_val = &false_vals[0];
+        }
+      } else {
+        return Err(format!("Invalid set of false values specified: \"{}\".", false_vals));
+      }
+      let key: &str = &keys[i];
+      if let Some(children) = doc.get_children(key) {
+        match children {
+          &Children::Count(ref c) => {
+            if c.get() > 0 {
+              result.push_str(&true_val);
+            } else {
+              result.push_str(&false_val);
+            }
+          },
+          &Children::Keys(ref ckeys) => {
+            if ckeys.borrow().len() > 0 {
+              result.push_str(&true_val);
+            } else {
+              result.push_str(&false_val);
+            }
+          }
+        }
+      } else {
+        result.push_str(&false_val);
       }
       if i < keys.len() - 1 {
         result.push_str(sep);
